db76677d0535308920201d65cc502e05852742db
Andrew Dinh <andrewdinh74@yahoo.com>
2021-08-19 14:04:47 -0700

    Remove unused items from import statements

--- focusApp/src/App.js
+++ focusApp/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Platform } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Platform } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { Focus } from "./src/features/focus/Focus";
